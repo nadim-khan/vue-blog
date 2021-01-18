cfb5492104fd9620da502572d780d32a53073e5c
nadim-khan <khan.nadim58@gmail.com>
2021-01-18 16:34:11 +0530

    data binding concept

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header @changeTitle="updateTitle($event)"/>
+    <H1 >{{title}}</H1>
     <Main msg="Main View Loaded" data="Nadeem"/>  </div>
+    
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import Header from './components/header'
 
 export default {
   name: 'App',
   components: {
-    Main
+    Main,
+    Header
+  },
+  data() {
+    return {
+      title:'title'
+    }
+  },
+  methods:{
+    updateTitle(title) {
+      this.title = title;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  margin:0px;
+  padding:0;
 }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+        <ul>
+            <li><img alt="Vue logo" src="../assets/logo.png"></li>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li><button @click="updateTitle">Update Title</button></li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Header',
+        methods:{
+            updateTitle() {
+                this.$emit('changeTitle','Hello vue JS');
+            }
+        }
+    }
+</script>
+<style scoped>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    li img{
+        width:30px;
+        height:30px;
+        padding:5px;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+</style>
